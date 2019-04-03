f436fe114e90859a6e837ecebbcf938ebe289324
Dinh Tran <robowonder@outlook.com>
2019-04-03 17:37:13 +0700

    create layout

--- src/layouts/components/Container/index.tsx
+++ src/layouts/components/Container/index.tsx
@@ -2,14 +2,14 @@ import * as React from 'react';
 import { observer } from 'mobx-react';
 import { ContainerStyled, StyledWrapper } from './style';
 import { SearchBox } from '../SearchBox';
-import { Suggestions } from '../Suggestions';
+import { Results } from '../Results';
 
 export const Container = observer(() => {
   return (
     <ContainerStyled>
       <StyledWrapper>
         <SearchBox />
-        <Suggestions visible={true} />
+        <Results visible={true} />
       </StyledWrapper>
     </ContainerStyled>
   );
