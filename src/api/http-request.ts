140abe6750f3ab9859844057e7b9b822d9a6d97b
Dinh Tran <robowonder@outlook.com>
2019-04-10 00:20:22 +0700

    .

--- src/api/http-request.ts
+++ src/api/http-request.ts
@@ -2,39 +2,44 @@ import * as http from 'http';
 import * as https from 'https';
 import { parse } from 'url';
 import console = require('console');
+import request = require('request');
+import FileCookieStore = require('tough-cookie-filestore');
+import { getPath } from '@vergo/shared/path';
 
 interface Data extends http.IncomingMessage {
   data: string;
 }
 
 export class HttpRequest {
-  private request;
-  initRequest(protocol: string) {
-    this.request = protocol.indexOf('s:') !== -1 ? https.request : http.request;
-  }
-
+  private cookie: any = {};
   load(url: string, Referer?: string, Cookie?: string, userAgent?: string) {
+    let _jar = request.jar(new FileCookieStore(getPath('cookies.json')));
+    let _request = request.defaults({ jar: _jar });
     const options: any = parse(url);
-    options.headers = {};
-    if (Referer) options.headers.Referer = Referer;
-    if (Cookie) options.headers.Cookie = Cookie;
-    this.initRequest(options.protocol);
+
+
+
+    /* if (!this.cookie.hasOwnProperty(options.host)) {
+
+    } */
+    // options.headers = {};
+    // if (Referer) options.headers.Referer = Referer;
+    // if (Cookie) options.headers.Cookie = Cookie;
+    // this.initRequest(options.protocol);
     return new Promise((resolve: (data: Data) => void, reject) => {
+      const req = request({ url, jar: _jar }, (error, res, body) => {
+        console.log(`${options.protocol}//${options.host}`);
+        console.log(_jar.getCookieString(`${options.protocol}//${options.host}`));
 
-      const req = this.request(options, res => {
-        let data = '';
-        console.log(res.headers);
-        res.setEncoding('binary');
-        res.on('data', chunk => {
-          data += chunk;
-        });
 
-        res.on('end', () => {
-          const d: any = { ...res, data };
-        resolve(d);
-        });
+      }).on('response', function (response) {
+        // unmodified http.IncomingMessage object
+        response.setEncoding('binary');
+        response.on('data', function (data) {
+          // compressed data as it is received
+          resolve({ data });
+        })
       });
-
       req.on('error', e => {
         reject(e);
       });
